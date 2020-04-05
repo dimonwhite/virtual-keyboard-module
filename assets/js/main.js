d2304a889882c646ea916d759a45f8b23bbf9963
dimonwhite <gen-tver@yandex.ru>
2020-04-05 23:45:38 +0300

    feat: add virtual-keyboard script

---
+++ assets/js/main.js
@@ -0,0 +1,881 @@
+const keyboard = [
+  [
+    {
+      key: '192',
+      text: {
+        ru: 'ё',
+        ruUp: 'Ё',
+        en: '`',
+        enUp: '~',
+      },
+      code: 'Backquote',
+    },
+    {
+      key: '49',
+      text: {
+        ru: '1',
+        ruUp: '!',
+        en: '1',
+        enUp: '!',
+      },
+      code: 'Digit1',
+    },
+    {
+      key: '50',
+      text: {
+        ru: '2',
+        ruUp: '"',
+        en: '2',
+        enUp: '@',
+      },
+      code: 'Digit2',
+    },
+    {
+      key: '51',
+      text: {
+        ru: '3',
+        ruUp: '№',
+        en: '3',
+        enUp: '#',
+      },
+      code: 'Digit3',
+    },
+    {
+      key: '52',
+      text: {
+        ru: '4',
+        ruUp: ';',
+        en: '4',
+        enUp: '$',
+      },
+      code: 'Digit4',
+    },
+    {
+      key: '53',
+      text: {
+        ru: '5',
+        ruUp: '%',
+        en: '5',
+        enUp: '%',
+      },
+      code: 'Digit5',
+    },
+    {
+      key: '54',
+      text: {
+        ru: '6',
+        ruUp: ':',
+        en: '6',
+        enUp: '^',
+      },
+      code: 'Digit6',
+    },
+    {
+      key: '55',
+      text: {
+        ru: '7',
+        ruUp: '?',
+        en: '7',
+        enUp: '&',
+      },
+      code: 'Digit7',
+    },
+    {
+      key: '56',
+      text: {
+        ru: '8',
+        ruUp: '*',
+        en: '8',
+        enUp: '*',
+      },
+      code: 'Digit8',
+    },
+    {
+      key: '57',
+      text: {
+        ru: '9',
+        ruUp: '(',
+        en: '9',
+        enUp: '(',
+      },
+      code: 'Digit9',
+    },
+    {
+      key: '48',
+      text: {
+        ru: '0',
+        ruUp: ')',
+        en: '0',
+        enUp: ')',
+      },
+      code: 'Digit0',
+    },
+    {
+      key: '189',
+      text: {
+        ru: '-',
+        ruUp: '_',
+        en: '-',
+        enUp: '_',
+      },
+      code: 'Minus',
+    },
+    {
+      key: '187',
+      text: {
+        ru: '=',
+        ruUp: '+',
+        en: '=',
+        enUp: '+',
+      },
+      code: 'Equal',
+    },
+    {
+      key: '8',
+      text: {
+        general: 'Backspace',
+      },
+      code: 'Backspace',
+      class: ['general', 'backspace'],
+    },
+  ],
+  [
+    {
+      key: '9',
+      text: {
+        general: 'Tab',
+      },
+      code: 'Tab',
+      class: ['general', 'tab'],
+    },
+    {
+      key: '81',
+      text: {
+        ru: 'й',
+        ruUp: 'Й',
+        en: 'q',
+        enUp: 'Q',
+      },
+      code: 'KeyQ',
+    },
+    {
+      key: '87',
+      text: {
+        ru: 'ц',
+        ruUp: 'Ц',
+        en: 'w',
+        enUp: 'W',
+      },
+      code: 'KeyW',
+    },
+    {
+      key: '69',
+      text: {
+        ru: 'у',
+        ruUp: 'У',
+        en: 'e',
+        enUp: 'E',
+      },
+      code: 'KeyE',
+    },
+    {
+      key: '82',
+      text: {
+        ru: 'к',
+        ruUp: 'К',
+        en: 'r',
+        enUp: 'R',
+      },
+      code: 'KeyR',
+    },
+    {
+      key: '84',
+      text: {
+        ru: 'е',
+        ruUp: 'Е',
+        en: 't',
+        enUp: 'T',
+      },
+      code: 'KeyT',
+    },
+    {
+      key: '89',
+      text: {
+        ru: 'н',
+        ruUp: 'Н',
+        en: 'y',
+        enUp: 'Y',
+      },
+      code: 'KeyY',
+    },
+    {
+      key: '85',
+      text: {
+        ru: 'г',
+        ruUp: 'Г',
+        en: 'u',
+        enUp: 'U',
+      },
+      code: 'KeyU',
+    },
+    {
+      key: '73',
+      text: {
+        ru: 'ш',
+        ruUp: 'Ш',
+        en: 'i',
+        enUp: 'I',
+      },
+      code: 'KeyI',
+    },
+    {
+      key: '79',
+      text: {
+        ru: 'щ',
+        ruUp: 'Щ',
+        en: 'o',
+        enUp: 'O',
+      },
+      code: 'KeyO',
+    },
+    {
+      key: '80',
+      text: {
+        ru: 'з',
+        ruUp: 'З',
+        en: 'p',
+        enUp: 'P',
+      },
+      code: 'KeyP',
+    },
+    {
+      key: '219',
+      text: {
+        ru: 'х',
+        ruUp: 'Х',
+        en: '[',
+        enUp: '{',
+      },
+      code: 'BracketLeft',
+    },
+    {
+      key: '221',
+      text: {
+        ru: 'ъ',
+        ruUp: 'Ъ',
+        en: ']',
+        enUp: '}',
+      },
+      code: 'BracketRight',
+    },
+    {
+      key: '220',
+      text: {
+        ru: '\\',
+        ruUp: '/',
+        en: '\\',
+        enUp: '|',
+      },
+      code: 'Backslash',
+    },
+    {
+      key: '46',
+      text: {
+        general: 'Del',
+      },
+      code: 'Delete',
+      class: ['general', 'del'],
+    },
+  ],
+  [
+    {
+      key: '20',
+      text: {
+        general: 'Caps Lock',
+      },
+      code: 'CapsLock',
+      class: ['general', 'capsLock'],
+    },
+    {
+      key: '65',
+      text: {
+        ru: 'ф',
+        ruUp: 'Ф',
+        en: 'a',
+        enUp: 'A',
+      },
+      code: 'KeyA',
+    },
+    {
+      key: '83',
+      text: {
+        ru: 'ы',
+        ruUp: 'Ы',
+        en: 's',
+        enUp: 'S',
+      },
+      code: 'KeyS',
+    },
+    {
+      key: '68',
+      text: {
+        ru: 'в',
+        ruUp: 'В',
+        en: 'd',
+        enUp: 'D',
+      },
+      code: 'KeyD',
+    },
+    {
+      key: '70',
+      text: {
+        ru: 'а',
+        ruUp: 'А',
+        en: 'f',
+        enUp: 'F',
+      },
+      code: 'KeyF',
+    },
+    {
+      key: '71',
+      text: {
+        ru: 'п',
+        ruUp: 'П',
+        en: 'g',
+        enUp: 'G',
+      },
+      code: 'KeyG',
+    },
+    {
+      key: '72',
+      text: {
+        ru: 'р',
+        ruUp: 'Р',
+        en: 'h',
+        enUp: 'H',
+      },
+      code: 'KeyH',
+    },
+    {
+      key: '74',
+      text: {
+        ru: 'о',
+        ruUp: 'О',
+        en: 'j',
+        enUp: 'J',
+      },
+      code: 'KeyJ',
+    },
+    {
+      key: '75',
+      text: {
+        ru: 'л',
+        ruUp: 'Л',
+        en: 'k',
+        enUp: 'K',
+      },
+      code: 'KeyK',
+    },
+    {
+      key: '76',
+      text: {
+        ru: 'д',
+        ruUp: 'Д',
+        en: 'l',
+        enUp: 'L',
+      },
+      code: 'KeyL',
+    },
+    {
+      key: '186',
+      text: {
+        ru: 'ж',
+        ruUp: 'Ж',
+        en: ';',
+        enUp: ':',
+      },
+      code: 'Semicolon',
+    },
+    {
+      key: '222',
+      text: {
+        ru: 'э',
+        ruUp: 'Э',
+        en: '\'',
+        enUp: '"',
+      },
+      code: 'Quote',
+    },
+    {
+      key: '13',
+      text: {
+        general: 'Enter',
+      },
+      code: 'Enter',
+      class: ['general', 'enter'],
+    },
+  ],
+  [
+    {
+      key: '16',
+      text: {
+        general: 'Shift',
+      },
+      code: 'ShiftLeft',
+      class: ['general', 'shift'],
+    },
+    {
+      key: '90',
+      text: {
+        ru: 'я',
+        ruUp: 'Я',
+        en: 'z',
+        enUp: 'Z',
+      },
+      code: 'KeyZ',
+    },
+    {
+      key: '88',
+      text: {
+        ru: 'ч',
+        ruUp: 'Ч',
+        en: 'x',
+        enUp: 'X',
+      },
+      code: 'KeyX',
+    },
+    {
+      key: '67',
+      text: {
+        ru: 'с',
+        ruUp: 'С',
+        en: 'c',
+        enUp: 'C',
+      },
+      code: 'KeyC',
+    },
+    {
+      key: '86',
+      text: {
+        ru: 'м',
+        ruUp: 'М',
+        en: 'v',
+        enUp: 'V',
+      },
+      code: 'KeyV',
+    },
+    {
+      key: '67',
+      text: {
+        ru: 'и',
+        ruUp: 'И',
+        en: 'b',
+        enUp: 'B',
+      },
+      code: 'KeyB',
+    },
+    {
+      key: '78',
+      text: {
+        ru: 'т',
+        ruUp: 'Т',
+        en: 'n',
+        enUp: 'N',
+      },
+      code: 'KeyN',
+    },
+    {
+      key: '77',
+      text: {
+        ru: 'ь',
+        ruUp: 'Ь',
+        en: 'm',
+        enUp: 'M',
+      },
+      code: 'KeyM',
+    },
+    {
+      key: '188',
+      text: {
+        ru: 'б',
+        ruUp: 'Б',
+        en: ',',
+        enUp: '<',
+      },
+      code: 'Comma',
+    },
+    {
+      key: '190',
+      text: {
+        ru: 'ю',
+        ruUp: 'Ю',
+        en: '.',
+        enUp: '>',
+      },
+      code: 'Period',
+    },
+    {
+      key: '191',
+      text: {
+        ru: '.',
+        ruUp: ',',
+        en: '/',
+        enUp: '?',
+      },
+      code: 'Slash',
+    },
+    {
+      key: '38',
+      text: {
+        arrow: '↑',
+      },
+      code: 'ArrowUp',
+      class: ['arrowUp'],
+    },
+    {
+      key: '38',
+      text: {
+        general: 'Shift',
+      },
+      code: 'ShiftRight',
+      class: ['general', 'shift'],
+    },
+  ],
+  [
+    {
+      key: '17',
+      text: {
+        general: 'Ctrl',
+      },
+      code: 'ControlLeft',
+      class: ['general', 'controlLeft'],
+    },
+    {
+      key: '91',
+      text: {
+        general: 'Win',
+      },
+      code: 'MetaLeft',
+      class: ['general', 'win'],
+    },
+    {
+      key: '18',
+      text: {
+        general: 'Alt',
+      },
+      code: 'AltLeft',
+      class: ['general', 'altLeft'],
+    },
+    {
+      key: '32',
+      text: {
+        general: '&nbsp;',
+      },
+      code: 'Space',
+      class: ['space'],
+    },
+    {
+      key: '18',
+      text: {
+        general: 'Alt',
+      },
+      code: 'AltRight',
+      class: ['general', 'altRight'],
+    },
+    {
+      key: '37',
+      text: {
+        general: '←',
+      },
+      code: 'ArrowLeft',
+      class: ['arrowLeft'],
+    },
+    {
+      key: '40',
+      text: {
+        arrow: '↓',
+      },
+      code: 'ArrowDown',
+      class: ['arrowDown'],
+    },
+    {
+      key: '39',
+      text: {
+        arrow: '→',
+      },
+      code: 'ArrowRight',
+      class: ['arrowRight'],
+    },
+    {
+      key: '17',
+      text: {
+        general: 'Ctrl',
+      },
+      code: 'ControlRight',
+      class: ['general', 'controlRight'],
+    },
+  ],
+];
+
+class Keyboard {
+  constructor(keys) {
+    this.keys = keys;
+    this.language = sessionStorage.getItem('language') ? sessionStorage.getItem('language') : 'ru';
+    this.body = document.querySelector('body');
+    this.keyboardBlock = document.createElement('div');
+    this.textarea = document.createElement('textarea');
+    this.textLanguage = document.createElement('div');
+    this.textWin = document.createElement('div');
+    this.up = false;
+    this.shiftDown = false;
+    this.capsDown = false;
+    this.languageDown = false;
+    this.textareaSelection = 0;
+  }
+
+  saveLanguage() {
+    sessionStorage.setItem('language', this.language);
+  }
+
+  toggleLanguage() {
+    this.language = this.language === 'ru' ? 'en' : 'ru';
+    this.keyboardBlock.classList.remove('ru', 'en');
+    this.keyboardBlock.classList.add(this.language);
+    this.saveLanguage();
+  }
+
+  editText(setting, text) {
+    let start;
+    let end;
+    switch (setting) {
+      case 'backspace':
+        this.textareaSelection = this.textarea.selectionStart;
+        if (this.textareaSelection !== 0) {
+          start = this.textarea.value.slice(0, this.textareaSelection - 1);
+          end = this.textarea.value.slice(this.textareaSelection);
+          this.textarea.value = start + end;
+          this.textarea.setSelectionRange(this.textareaSelection - 1, this.textareaSelection - 1);
+        }
+        break;
+      case 'delete':
+        this.textareaSelection = this.textarea.selectionStart;
+        start = this.textarea.value.slice(0, this.textareaSelection);
+        end = this.textarea.value.slice(this.textareaSelection + 1);
+        this.textarea.value = start + end;
+        this.textarea.setSelectionRange(this.textareaSelection, this.textareaSelection);
+        break;
+      case 'tab':
+        this.textareaSelection = this.textarea.selectionStart;
+        start = this.textarea.value.slice(0, this.textareaSelection);
+        end = this.textarea.value.slice(this.textareaSelection);
+        this.textarea.value = `${start}    ${end}`;
+        this.textarea.setSelectionRange(this.textareaSelection + 4, this.textareaSelection + 4);
+        break;
+      default:
+        this.textareaSelection = this.textarea.selectionStart;
+        start = this.textarea.value.slice(0, this.textareaSelection);
+        end = this.textarea.value.slice(this.textareaSelection);
+        this.textarea.value = start + text + end;
+        this.textarea.setSelectionRange(this.textareaSelection + 1, this.textareaSelection + 1);
+        break;
+    }
+  }
+
+  toggleUp() {
+    this.up = !this.up;
+    if (this.up) {
+      this.keyboardBlock.classList.add('up');
+    } else {
+      this.keyboardBlock.classList.remove('up');
+    }
+  }
+
+  keydown(e) {
+    e.preventDefault();
+    const btn = this.keyboardBlock.querySelector(`.${e.code}`);
+
+    if (btn) {
+      if (e.code !== 'CapsLock') {
+        btn.classList.add('active');
+      }
+      if (btn.classList.contains('general')) {
+        switch (e.code) {
+          case 'ControlLeft':
+          case 'ControlRight':
+            if (e.ctrlKey === true && e.shiftKey === true && !this.languageDown) {
+              this.languageDown = true;
+              this.toggleLanguage();
+            }
+            break;
+          case 'Backspace':
+            this.editText('backspace');
+            break;
+          case 'Delete':
+            this.editText('delete');
+            break;
+          case 'Tab':
+            this.editText('tab');
+            break;
+          case 'Enter':
+            this.editText('text', '\n');
+            break;
+          case 'CapsLock':
+            if (!this.capsDown) {
+              this.capsDown = true;
+              btn.classList.toggle('active');
+              this.toggleUp();
+            }
+            break;
+          case 'ShiftLeft':
+          case 'ShiftRight':
+            if (!this.shiftDown) {
+              this.shiftDown = true;
+              this.toggleUp();
+            }
+            if (e.ctrlKey === true && e.shiftKey === true && !this.languageDown) {
+              this.languageDown = true;
+              this.toggleLanguage();
+            }
+            break;
+          default:
+            break;
+        }
+        return;
+      }
+      this.editText('text', btn.innerText);
+    }
+  }
+
+  keyup(e) {
+    e.preventDefault();
+    const btn = this.keyboardBlock.querySelector(`.${e.code}`);
+    if (btn) {
+      switch (e.code) {
+        case 'ControlLeft':
+        case 'ControlRight':
+          btn.classList.remove('active');
+          this.languageDown = false;
+          break;
+        case 'CapsLock':
+          this.capsDown = false;
+          break;
+        case 'ShiftLeft':
+        case 'ShiftRight':
+          btn.classList.remove('active');
+          this.shiftDown = false;
+          this.toggleUp();
+          break;
+        default:
+          btn.classList.remove('active');
+          break;
+      }
+    }
+  }
+
+  mousedown(e) {
+    e.preventDefault();
+    const btn = e.target.closest('.keyboard_item');
+    if (btn) {
+      if (!btn.classList.contains('CapsLock')) {
+        btn.classList.add('active');
+      }
+      if (btn.classList.contains('general')) {
+        switch (true) {
+          case btn.classList.contains('Backspace'):
+            this.editText('backspace');
+            break;
+          case btn.classList.contains('Delete'):
+            this.editText('delete');
+            break;
+          case btn.classList.contains('Tab'):
+            this.editText('tab');
+            break;
+          case btn.classList.contains('Enter'):
+            this.editText('text', '\n');
+            break;
+          case btn.classList.contains('CapsLock'):
+            this.capsDown = true;
+            btn.classList.toggle('active');
+            this.toggleUp();
+            break;
+          case btn.classList.contains('ShiftLeft'):
+          case btn.classList.contains('ShiftRight'):
+            this.toggleUp();
+            break;
+          default:
+            break;
+        }
+        return;
+      }
+      this.editText('text', btn.innerText);
+    }
+  }
+
+  removeActiveClass(e) {
+    e.preventDefault();
+    this.keyboardBlock.querySelectorAll('.keyboard_item.active').forEach((item) => {
+      if (!item.classList.contains('CapsLock')) {
+        item.classList.remove('active');
+      }
+      if ((item.classList.contains('ShiftLeft') || item.classList.contains('ShiftRight')) && (!this.capsDown || this.shiftDown) && (!this.capsDown && !this.shiftDown)) {
+        this.toggleUp();
+      }
+    });
+  }
+
+  init() {
+    this.textLanguage.classList.add('text');
+    this.textLanguage.innerText = 'Клавиатура создана в операционной системе Windows';
+    this.textWin.classList.add('text');
+    this.textWin.innerText = 'Для переключения языка используйте комбинацию ctrl + shift';
+
+    this.keyboardBlock.classList.add('keyboard', this.language);
+    this.textarea.classList.add('textarea');
+    this.body.append(this.textarea);
+    this.body.append(this.keyboardBlock);
+    this.body.append(this.textLanguage);
+    this.body.append(this.textWin);
+
+    this.keys.forEach((row) => {
+      const rowElement = document.createElement('div');
+      rowElement.classList.add('keyboard_row');
+      row.forEach((item) => {
+        const btn = document.createElement('div');
+        btn.classList.add('keyboard_item');
+        btn.classList.add(item.code);
+        if (item.class) {
+          btn.classList.add(...item.class);
+        }
+        const { text } = item;
+
+        Object.keys(text).forEach((key) => {
+          btn.innerHTML += `<span class="${key}">${text[key]}</span>`;
+        });
+        rowElement.append(btn);
+      });
+      this.keyboardBlock.append(rowElement);
+    });
+
+    document.addEventListener('keydown', (e) => {
+      this.keydown(e);
+    });
+
+    document.addEventListener('keyup', (e) => {
+      this.keyup(e);
+    });
+
+    this.keyboardBlock.addEventListener('mousedown', (e) => {
+      this.mousedown(e);
+    });
+
+    document.addEventListener('mouseup', (e) => {
+      this.removeActiveClass(e);
+    });
+
+    this.saveLanguage();
+  }
+}
+
+const virtualKeyboard = new Keyboard(keyboard);
+
+virtualKeyboard.init();
